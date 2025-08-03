0dfabad504bddc1c121242c0e558e9ff9e9aa2a4
WildHonestFur <137202612+WildHonestFur@users.noreply.github.com>
2025-08-03 20:00:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -299,4 +299,5 @@ def done():
     return render_template("thanks.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 1000))
+    app.run(host="0.0.0.0", port=port)
